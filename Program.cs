b5cb8bb86bc0f6bba1d07fb99cef14ddd8013ce5
Esraa-la <850009158@nzse.ac.nz>
2026-05-22 20:00:33 +1200

    Added average score calculator

--- Program.cs
+++ Program.cs
@@ -18,6 +18,12 @@ namespace GradeCalculator_Esra
             else grade = "F";
 
             Console.WriteLine($"Your grade is: {grade}");
+            Console.Write("Enter number of assignments: ");
+            int assignments = int.Parse(Console.ReadLine());
+
+            double average = (double)score / assignments;
+
+            Console.WriteLine($"Average score: {average:F2}");
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
